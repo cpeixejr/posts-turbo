818c5faca896f8c1a8967317b82fedaccb449933
Carlos <cpeixejr@gmail.com>
2024-05-29 23:25:02 -0300

    feat: adiciona brodcasts a lista de posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
-    scope :ordered, -> { order(id: :desc) }
-    validates :title, :body, presence: true
+  scope :ordered, -> { order(id: :desc) }
+  validates :title, :body, presence: true
+
+  broadcasts_to -> (post) { "posts" }, inserts_by: :prepend
 end
